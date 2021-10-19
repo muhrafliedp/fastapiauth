b53f1b1c9f7e74a702970cb1350b35697bc322e4
muhrafliedp <18219035@std.stei.itb.ac.id>
2021-10-19 23:51:53 +0700

    Commit ketiga. Authorize hanya memasukkan Token saja. Namun token yang sesuai hanya bisa didapatkan dari Proses Get User, lalu Generate Token.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 import jwt
 
-from fastapi import FastAPI, Depends, HTTPException, status 
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, HTTPException, Request, status 
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials, OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
@@ -11,15 +11,50 @@ with open("menu.json", "r") as read_file:
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+JWT_SECRET = "Raflie's Signature"
+
+USER_PAYLOAD = {
+    "username": "asdf", 
+    "password": "asdf"
+    }
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="You don't have access here because your token is invalid or expired.")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str) -> bool:
+        isTokenValid: bool = False
+        try:
+            payload = jwt.decode(jwtoken, JWT_SECRET, algorithms=['HS256'])
+        except:
+            payload = None
+        if payload == USER_PAYLOAD:
+            isTokenValid = True
+        return isTokenValid
 
 async def authenticate_user(username: str, password: str):
-    # user = await User.get(username=username)
     if username != "asdf":
         return False 
     if password != "asdf":
         return False
     return True 
+
+# ~~~ UNPROTECTED ~~~ #
+
+@app.get('/')
+async def get_user():
+    return USER_PAYLOAD
 
 @app.post('/token')
 async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -30,29 +65,22 @@ async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail='Invalid username or password'
         )
-
-    token = jwt.encode({"username":form_data.username, "password": form_data.password}, "Raflie's Signature")
+    payload = {
+        "username": form_data.username, 
+        "password": form_data.password
+    }
+    token = jwt.encode(payload, JWT_SECRET)
 
     return {'access_token' : token, 'token_type' : 'bearer'}
 
-@app.get('/')
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, "Raflie's Signature", algorithms=['HS256'])
+# ~~~ PROTECTED ~~~ #
 
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail='Invalid username or password'
-        )
-    return payload
-
-@app.get('/menu')
-async def read_all_menu(token: str = Depends(oauth2_scheme)):
+@app.get('/menu', dependencies=[Depends(JWTBearer())])
+async def read_all_menu():
     return data['menu']
 
-@app.get('/menu/{item_id}') 
-async def read_menu(item_id:int, token: str = Depends(oauth2_scheme)): 
+@app.get('/menu/{item_id}', dependencies=[Depends(JWTBearer())]) 
+async def read_menu(item_id:int): 
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             return menu_item
@@ -60,8 +88,8 @@ async def read_menu(item_id:int, token: str = Depends(oauth2_scheme)):
         status_code=404, detail=f'Item not found'
     )
 
-@app.post('/menu/{item_name}')
-async def add_menu(item_name:str, token: str = Depends(oauth2_scheme)):
+@app.post('/menu/{item_name}', dependencies=[Depends(JWTBearer())])
+async def add_menu(item_name:str):
     id=1
     if(len(data['menu'])>0):
         id=data['menu'][len(data['menu'])-1]['id']+1
@@ -78,8 +106,8 @@ async def add_menu(item_name:str, token: str = Depends(oauth2_scheme)):
         status_code=500, detail=f'Data Gagal Diperbarui'
     )
 
-@app.put('/menu/{item_id}')
-async def update_menu(item_id:int, item_name:str, token: str = Depends(oauth2_scheme)):
+@app.put('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
+async def update_menu(item_id:int, item_name:str):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             menu_item['name'] = item_name
@@ -95,8 +123,8 @@ async def update_menu(item_id:int, item_name:str, token: str = Depends(oauth2_sc
         status_code=404, detail=f'Item not found'
     )
 
-@app.delete('/menu/{item_id}')
-async def delete_menu(item_id:int, token: str = Depends(oauth2_scheme)):
+@app.delete('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
+async def delete_menu(item_id:int):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             data['menu'].remove(menu_item)
